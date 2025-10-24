d76108d1fec14fd06c322baa5aac6942016aabf9
Priscilla Andresen <pgdamorim@gmail.com>
2025-10-24 14:07:30 -0400

    adds stuff

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+from flask import abort, Blueprint, make_response
 from app.models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-@planets_bp.get_all_planets('')
+@planets_bp.get('')
 def get_all_planets():
     result = []
 
@@ -16,3 +16,33 @@ def get_all_planets():
         ))
 
     return result
+
+@planets_bp.get('/<id>')
+def get_single_planet(id):
+
+    planet = validate_planet(id)
+
+    planet_dict = dict (
+        id=planet.id,
+        name=planet.name,
+        description=planet.description,
+        num_of_moons=planet.num_of_moons
+    )
+    
+    return planet_dict
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except ValueError:
+        response = {'message': f'planet id {id} is invalid'}
+
+        abort(make_response(response, 400))
+
+    for planet in planets:
+        if planet.id == id:
+            return planet
+    
+    not_found = {'message': f'planet with id {id} not found'}
+    abort(make_response(not_found, 404))
+
